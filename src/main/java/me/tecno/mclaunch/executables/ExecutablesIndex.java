7140100d018ee40ec41bd2e8a8d7bc2f07ce6f1d
MRtecno98 <mr.tecno98@gmail.com>
2023-08-31 11:51:23 +0200

    Add executables index deserialization

--- src/main/java/me/tecno/mclaunch/executables/ExecutablesIndex.java
+++ src/main/java/me/tecno/mclaunch/executables/ExecutablesIndex.java
@@ -1,5 +1,13 @@
 package me.tecno.mclaunch.executables;
 
-public class ExecutablesIndex {
+import com.google.gson.annotations.SerializedName;
+import me.tecno.mclaunch.structure.FileDataset;
 
+public record ExecutablesIndex(
+		FileDataset client,
+		@SerializedName("client_mappings")
+		FileDataset clientMappings,
+		FileDataset server,
+		@SerializedName("server_mappings")
+		FileDataset serverMappings) {
 }
