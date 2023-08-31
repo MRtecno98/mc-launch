9a053740dbf8322e7660ab923c37a999f15c1752
MRtecno98 <mr.tecno98@gmail.com>
2023-08-31 11:50:43 +0200

    Add library registry

--- src/main/java/me/tecno/mclaunch/libraries/Library.java
+++ src/main/java/me/tecno/mclaunch/libraries/Library.java
@@ -1,5 +1,22 @@
 package me.tecno.mclaunch.libraries;
 
-public class Library {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import me.tecno.mclaunch.structure.PathFileDataset;
 
+import java.net.URI;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Library {
+	private final String name;
+	private final LibraryDownloads downloads;
+
+	private URI repository;
+
+	public record LibraryDownloads(PathFileDataset artifact,
+								   Map<String, PathFileDataset> classifiers) {}
 }
